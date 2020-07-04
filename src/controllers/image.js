0434c836b9bac50e60d865111f8f3afa4b9c4a04
Omarz98 <zaro9812@gmail.com>
2020-07-04 16:50:28 -0500

    subida de imagenes

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,11 +1,26 @@
+const path = require('path');
+const { randomNumber } = require('../helpers/libs');
+const fs = require('fs-extra');
+
 const ctrl = {}
 
 ctrl.index = (req, res) =>{
 
 };
 
-ctrl.create = (req, res) =>{
+ctrl.create = async (req, res) =>{
+    //console.log(req.file);
+    const imgUrl = randomNumber();
+    console.log(imgUrl);
+    const imageTemPath = req.file.path;
+    const ext = path.extname(req.file.originalname).toLowerCase();
+    const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
     
+    if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+        await fs.rename(imageTemPath, targetPath);    
+    }
+
+    res.send('Works!');
 };
 
 ctrl.like = (req, res) => {
